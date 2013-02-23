151539bc7ac479483f6bae4bea203332912e2bb6
Paul Giberson <ak@aknosis.com>
2013-02-22 22:10:40 -0700

    add input checks add security checks

--- myWebHook.php
+++ myWebHook.php
@@ -20,6 +20,41 @@ $log = FALSE;
 # ==== No Humans Below. Only Honey Badgers. http://youtu.be/4r7wHMg5Yjg =====
 # ===========================================================================
 
+$ip = $_SERVER['REMOTE_ADDR'];
+$protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
+
+if (empty($ip)) {
+  header($protocol.' 400 Bad Request');
+  die('invalid ip saddress');
+} elseif (empty($_POST['payload'])) {
+  header($protocol.' 400 Bad Request');
+  die('missing payload');
+}
+
+//Bitbucket
+//https://confluence.atlassian.com/display/BITBUCKET/POST+Service+Management
+$bitbucket = array('63.246.22.222');
+
+//Github
+//https://github.com/<user>/<repo>/settings/hooks
+$github = array(
+  '207.97.227.253',
+  '50.57.128.197',
+  '108.171.174.178',
+  '50.57.231.61',
+  '54.235.183.49',
+  '54.235.183.23',
+  '54.235.118.251',
+  '54.235.120.57',
+  '54.235.120.61',
+  '54.235.120.62'
+);
+
+if (!in_array($ip, array_merge($bitbucket, $github))) {
+  header($protocol.' 403 Forbidden');
+  die('invalid ip address');
+}
+
 # Receive POST data
 $payload = json_decode($_POST['payload'], true);
 
@@ -37,7 +72,7 @@ if($log) {
 if( isset($payload['canon_url']) && strpos($payload['canon_url'],"bitbucket")!== FALSE ) {
   $branch = isset($payload['commits'][0]['branches']) ? $payload['commits'][0]['branches'][0]:$payload['commits'][0]['branch'];
 } else if( isset($payload['repository']['url']) && strpos($payload['repository']['url'],"github")!==FALSE ) {
-  $branch = str_replace("refs/heads/","",$payload['ref']); 
+  $branch = str_replace("refs/heads/","",$payload['ref']);
 } else {
   $branch = "NO-BRANCH-DETECTED-DO-NOT-CREATE-A-FOLDER-WITH-THIS-NAME";
 }
@@ -60,7 +95,7 @@ if( array_key_exists($branch,$aliases) ) {
     $list = array_filter($list,"filterNonDir");
     # merge with existing directories
     $dirs_to_update = array_merge($dirs_to_update,$list);
-  } 
+  }
   else if(is_dir($aliases[$branch])) {
     $dirs_to_update[] = $aliases[$branch];
   }
@@ -71,7 +106,7 @@ if( empty($dirs_to_update) ) {
   die("Apparently there is nothing to update for this branch\n");
 }
 
-# Capture current directory 
+# Capture current directory
 $original_dir = getcwd();
 
 # Loop through the directories
